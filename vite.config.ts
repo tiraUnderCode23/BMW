4366e7d07e42b367101c4ca8236351684a4cca73
tiraUnderCode23 <10usmailme@gmail.com>
2025-07-14 22:40:53 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react"
 import { defineConfig } from "vite"
 
 export default defineConfig({
-  base: "/BMW/", // <--- مهم جداً لعمل GitHub Pages
+  // السطر الذي كان هنا تم حذفه
   plugins: [react()],
   resolve: {
     alias: {
